8e9a845d2e7de18d0a9343a3bbbf4d1ef9caa7b2
lx-cly <63039798+lx-cly@users.noreply.github.com>
2022-10-28 16:46:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ def train(hyp, opt, device, tb_writer=None):
     else:
         lf = one_cycle(1, hyp['lrf'], epochs)  # cosine 1->hyp['lrf']
     scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
-    plot_lr_scheduler(optimizer, scheduler, epochs)
+    plot_lr_scheduler(optimizer, scheduler, epochs, save_dir=save_dir)
 
     # EMA
     ema = ModelEMA(model) if rank in [-1, 0] else None
