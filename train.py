2ee2392f00ab7cc36dbf69597114c4be81b84612
lx-cly <63039798+lx-cly@users.noreply.github.com>
2022-12-01 15:33:12 +0800

    Update train.py

--- train.py
+++ train.py
@@ -255,7 +255,7 @@ def train(hyp, opt, device, tb_writer=None):
     # Process 0
     if rank in [-1, 0]:
         testloader = create_dataloader(test_path, imgsz_test, batch_size * 2, gs, opt, names, # testloader
-                                       hyp=hyp, cache=opt.cache_images and not opt.notest, rect=True, rank=-1,
+                                       hyp=hyp, cache=opt.cache_images and not opt.notest, rect=False, rank=-1,
                                        world_size=opt.world_size, workers=opt.workers,
                                        pad=0.5, prefix=colorstr('val: '))[0]
 
